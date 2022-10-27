cef889e8019026ae085b6155d7e3f1f34977a2f3
Rafa Moral <rafael.moral@adevinta.com>
2022-10-27 11:23:18 +0200

    brew packages script

---
+++ commands/index.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# get current location
+ROOT_DIR=$1
+CONFIG_DIR="$ROOT_DIR/configs"
+SCRIPTS_DIR="$ROOT_DIR/scripts"
+
+# load utils
+source "$ROOT_DIR/commands/__util.sh"
+
+echo "Running commands/install.sh..."
+
+
+# *************************************
+# oh-my-zsh
+#
+log_section_start "Installing oh-my-zsh"
+
+if [ -d ~/.oh-my-zsh/ ]; then
+  echo "Cleaning up ~/.oh-my-zsh/"
+  rm -rf ~/.oh-my-zsh/
+fi
+
+RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# *************************************
+# Other scripts
+
+bash "$SCRIPTS_DIR/brew.sh" "$ROOT_DIR"
+
+# *************************************
+# End
+#
+echo "Everything is done!"
---
+++ scripts/brew.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+echo "#############################"
+echo "# Installing various programs from brew"
+echo "#############################"
+echo ""
+
+ROOT_DIR=$1
+source "$ROOT_DIR/commands/__util.sh"
+
+brew -v >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Update zsh
+brew_install_or_upgrade zsh
+
+# set zsh as default shell
+chsh -s /bin/zsh
+
+# Install brew packages
+brew_install_or_upgrade git
+brew_install_or_upgrade git-extras
+brew_install_or_upgrade curl
+brew_install_or_upgrade docker
