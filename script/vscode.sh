e8cad5348f03f416909961421284c6d3ab692432
Rafa Moral <rafael.moral@adevinta.com>
2022-12-14 16:22:44 +0100

    Fix extensions file path

--- script/vscode.sh
+++ script/vscode.sh
@@ -10,4 +10,4 @@ ROOT_DIR=$1
 CONFIG_DIR="$ROOT_DIR/config/vscode"
 
 echo "Install VS Code extensions"
-< "$CONFIG_DIR/package-lists/visual-studio-code-extensions.txt" xargs -L 1 code --install-extension
+< "./$CONFIG_DIR/extensions.txt" xargs -L 1 code --install-extension
